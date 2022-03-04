b84f5f60248d4e864396dbecf1662e8a1b4f318a
bturner <bturner@bren-tronics.com>
2022-03-04 12:05:38 -0500

    initial commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,5 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map',
-    optimization: {
-        splitChunks: {
-            // include all types of chunks
-            chunks: 'all'
-        }
-    }
+    devtool: 'source-map'
 });
